b3d1f780f3397910f67a141082f75e3ba76c0c74
ilyasbaggelaar <ilyas.baggelaar@gmail.com>
2025-01-13 16:47:33 +0000

    Firebase fix

--- app.ts
+++ app.ts
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use("/api/legislationSummaries", legislationRoutes);
 
 
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
