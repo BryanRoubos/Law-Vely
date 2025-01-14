3b83ef967b0c6c457bfc55ec2a773f2df0478fcd
ilyasbaggelaar <ilyas.baggelaar@gmail.com>
2025-01-14 14:24:00 +0000

    updated project structure

--- dist/jest.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-module.exports = {
-    preset: "ts-jest", // Use ts-jest for TypeScript support
-    testEnvironment: "node", // Set the test environment to Node.js
-    transform: {
-        "^.+\\.tsx?$": "ts-jest", // Transform TypeScript files
-    },
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-};
