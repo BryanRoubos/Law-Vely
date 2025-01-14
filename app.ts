01892a28fc389ef47cc21229f466d72729b7e76f
ilyasbaggelaar <ilyas.baggelaar@gmail.com>
2025-01-14 10:47:10 +0000

    installed and set up cors

--- app.ts
+++ app.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import { Request } from "express"
 import legislationRoutes from "./routes/legislationRoutes";
+import cors from "cors";
 
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/legislationSummaries", legislationRoutes);
 
