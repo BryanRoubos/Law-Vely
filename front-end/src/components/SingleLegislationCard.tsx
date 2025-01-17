43182709c26452c35ef48f31dbcd87ef9736cf3c
Janilee <janileesvardstal@gmail.com>
2025-01-17 16:50:09 +0000

    adds url link to legislation

--- front-end/src/components/SingleLegislationCard.tsx
+++ front-end/src/components/SingleLegislationCard.tsx
@@ -2,7 +2,7 @@ import ReactMarkdown from "react-markdown";
 import { manipulateDateAndTime } from "../utils/utils";
 import ReportPopup from "./ReportPopup";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBookmark as solidBookmark } from "@fortawesome/free-solid-svg-icons";
+import { faChevronDown, faChevronUp, faBookmark as solidBookmark } from "@fortawesome/free-solid-svg-icons";
 import { faBookmark as regularBookmark } from "@fortawesome/free-regular-svg-icons";
 import Button from "@mui/material/Button";
 import { useState } from "react";
@@ -13,6 +13,7 @@ interface SingleLegislation {
   summaryOfSubSections: string;
   timestamp: number;
   title: string;
+  url: string;
 }
 
 interface SingleLegislationCardProps {
@@ -21,6 +22,7 @@ interface SingleLegislationCardProps {
 
 function SingleLegislationCard({ legislation }: SingleLegislationCardProps) {
   const [isTrackedClicked, setIsTrackedClicked] = useState(false);
+  const [showSubSections, setShowSubSections] = useState(false);
 
   return (
     <div id="SLC-1" className="md:mx-8">
@@ -31,26 +33,54 @@ function SingleLegislationCard({ legislation }: SingleLegislationCardProps) {
         <strong>Summary</strong>
       </h2>
       <p id="SLC-3" className="text-gray-600 mb-4">{legislation.summaryOfLegislation}</p>
-      <h2>
-        <strong>Sub-Section Summaries</strong>
-      </h2>
-      <ReactMarkdown className="text-gray-600 mb-4">
-        {legislation.summaryOfSubSections}
-      </ReactMarkdown>
-      <p id="SLC-4" className="text-sm text-gray-500">
+      
+      <div>
+      <Button
+        id="subsect-btn"
+        variant="text"
+        onClick={() => setShowSubSections(!showSubSections)}
+        className="flex items-center text-blue-600"
+      >
+        {showSubSections ? (
+          <>
+          Show Less <FontAwesomeIcon icon={faChevronUp} className="ml-2" />
+        </>
+    ) : (
+    <>
+      Show More <FontAwesomeIcon icon={faChevronDown} className="ml-2" />
+    </>
+  )}
+</Button>
+        
+        {showSubSections && (
+          <div>
+            <h2>Subsection Summaries</h2>
+          <ReactMarkdown className="text-gray-600 mb-4">
+            {legislation.summaryOfSubSections}
+          </ReactMarkdown>
+          </div>
+        )}
+      </div>
+      
+      <p id="SLC-4" className="text-sm text-gray-500 m-1">
         Date Created: {manipulateDateAndTime(legislation.timestamp)}
       </p>
-      <a href="">Find more information here...</a>
+      <a href={legislation.url}>Find more information here...</a>
       <div id="SLC-5" className="flex justify-between">
-      <Button id="track-btn" variant="contained" type="button" onClick={() => setIsTrackedClicked(!isTrackedClicked)}>
-        <FontAwesomeIcon
-              icon={isTrackedClicked ? solidBookmark : regularBookmark} className="mr-2" 
-            />
-
-            {isTrackedClicked ? `Tracked` : `Track`}
-      </Button>
-            <ReportPopup />
-          </div>
+        <Button
+          id="track-btn"
+          variant="contained"
+          type="button"
+          onClick={() => setIsTrackedClicked(!isTrackedClicked)}
+        >
+          <FontAwesomeIcon
+            icon={isTrackedClicked ? solidBookmark : regularBookmark}
+            className="mr-2"
+          />
+          {isTrackedClicked ? `Tracked` : `Track`}
+        </Button>
+        <ReportPopup />
+      </div>
     </div>
   );
 }
