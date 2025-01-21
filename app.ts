074ba6c1c1a230d33b894eb223f7865097ac2e34
Emilybrown245 <emilybrown@Emilys-MacBook-Pro.local>
2025-01-21 17:41:19 +0000

    function to allow multiple category queries at once

--- app.ts
+++ app.ts
@@ -11,7 +11,7 @@ app.use(cors());
 app.use("/api/legislationSummaries", legislationRoutes);
 app.use("/api/auth", authRoutes)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(
     `Server is running on port  http://localhost:${PORT}/api/legislationSummaries`
