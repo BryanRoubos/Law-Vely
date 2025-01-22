137f1ad47888b4040bbad2f876ded31a42ef6f4c
Janilee <janileesvardstal@gmail.com>
2025-01-22 15:50:21 +0000

    general styling

--- front-end/src/components/Logo.tsx
+++ front-end/src/components/Logo.tsx
@@ -1,19 +1,22 @@
-import logo from "../assets/8-logo-no-book--white-congenial-2.svg";
-import { motion } from "framer-motion";
+import logo from '../assets/logo-white-min-shadow.png';
+// import { motion } from "framer-motion";
 
 function Logo() {
    return (
-     <motion.div
-       initial={{ opacity: 0, scale: 1.8 }}
-       animate={{
-         opacity: 1,
-         scale: 0.5 + (window.innerWidth / 1440) * 0.5,
-       }}
-       transition={{ duration: 0.4, ease: "easeInOut" }}
-       whileHover={{ scale: 1.1, rotate: -3 }}
-     >
-       <img src={logo} alt="Law-Vely Logo" className="h-auto max-h-12 md:max-h-16 w-auto max-w-full mt-1" />
-     </motion.div>
+    //  <motion.div
+    //    initial={{ opacity: 0, scale: 1.8 }}
+    //    animate={{
+    //      opacity: 1,
+    //      scale: 0.5 + (window.innerWidth / 1440) * 0.5,
+    //    }}
+    //    transition={{ duration: 0.4, ease: "easeInOut" }}
+    //    whileHover={{ scale: 1.1, rotate: -3 }}
+    //  >
+    //    <img src={logo} alt="Law-Vely Logo" className="h-auto max-h-12 md:max-h-16 w-auto max-w-full mt-1" />
+    //  </motion.div>
+    <div> 
+      <img src={logo} alt="Law-Vely Logo" className="h-auto max-h-8 md:max-h-12 w-auto max-w-full mt-1" />
+    </div>
    );
  }
  
